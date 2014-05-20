8ed6de8b4a2f8f28cb8646b0e1b8e1268430aae6
Jiri Peinlich <jiri.peinlich@gmail.com>
2014-05-20 05:36:05 +0200

    adding javadoc to test

--- src/test/java/cz/peinlich/exam/ApplicationTests.java
+++ src/test/java/cz/peinlich/exam/ApplicationTests.java
@@ -6,6 +6,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * This class just loads the application context. It is responsible for testing that the
+ * application context is properly created in tests. The class was generated using
+ * start.spring.io.
+ * <p/>
+ * User: George
+ * Date: 20.5.2014
+ * Time: 5:33
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
